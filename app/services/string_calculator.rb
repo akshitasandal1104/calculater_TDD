3c53a13bb2f1f94b8cbddefd3147f1cb9be821b8
Akshita Sandal <asandal@oneims.com>
2024-08-05 22:41:59 +0530

    # Empty String will return 0 return a number for Single number Input. Wrote the code in the service

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,7 +1,12 @@
 class StringCalculator
-  def add(numbers)
-    return 0 if numbers.empty?
-    
-    numbers.split(',').map(&:to_i).sum
-  end
+    def add(numbers)
+      return 0 if numbers.empty?
+
+  
+      nums = numbers.split(delimiter).map(&:to_i)
+      negatives = nums.select { |n| n < 0 }
+      raise "Negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
+  
+      nums.sum
+    end
 end
--- spec/spec/services/string_calculator_spec.rb
+++ spec/spec/services/string_calculator_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 describe StringCalculator do
   describe '#add' do
+    it 'returns 0 for an empty string' do
+      expect(subject.add("")).to eq(0)
+    end
 
+    it 'returns the number for a single number input' do
+      expect(subject.add("1")).to eq(1)
+    end
+
+    
   end
 end
