e214a1c7bb489d636345cff026a098b77ee1da73
Akshita Sandal <asandal@oneims.com>
2024-08-05 23:04:55 +0530

    Added Logic for New Lines as Delimeters. Added Logic that support custom Delimeters.

--- spec/spec/services/string_calculator_spec.rb
+++ spec/spec/services/string_calculator_spec.rb
@@ -18,5 +18,14 @@ describe StringCalculator do
       expect(subject.add("1,2,3")).to eq(6)
     end
 
+    it 'handles new lines as delimiters' do
+      expect(subject.add("1\n2,3")).to eq(6)
+    end
+
+    it 'supports custom delimiters' do
+      expect(subject.add("//;\n1;2")).to eq(3)
+    end
+
+   
   end
 end
