a53895022033b9986c46df2b323b863dee5c13ca
Akshita Sandal <asandal@oneims.com>
2024-08-05 22:43:20 +0530

    Added Logic  to change the Delimeter.

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,7 +1,13 @@
 class StringCalculator
     def add(numbers)
       return 0 if numbers.empty?
-
+  
+      delimiter = /,|\n/
+      if numbers.start_with?("//")
+        custom_delimiter = numbers[2]
+        numbers = numbers[4..]
+        delimiter = /#{custom_delimiter}|,|\n/
+      end
   
       nums = numbers.split(delimiter).map(&:to_i)
       negatives = nums.select { |n| n < 0 }
